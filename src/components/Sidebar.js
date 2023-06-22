50cd75e81de6d138ef0127fb68a8cf4062b3a740
Justin <justin@vinova.com.sg>
2023-06-22 17:28:52 +0700

    hero checkout

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,26 +9,29 @@ import CartButtons from './CartButtons';
 import { useUserContext } from '../context/user_context';
 
 const Sidebar = () => {
-	const data = useProductsContext();
-	console.log('data:', data);
-	const isOpen = false;
+	const { isSidebarOpen, closeSidebar } = useProductsContext();
+
 	return (
 		<SidebarContainer>
-			<aside className={`${isOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
+			<aside className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
 				<div className='sidebar-header'>
 					<img src={logo} alt='logo' className='logo' />
-					<button className='close-btn' type='button'>
+					<button className='close-btn' type='button' onClick={closeSidebar}>
 						<FaTimes />
 					</button>
 				</div>
 				<ul className='links'>
 					{links.map(({ id, text, url }) => (
 						<li key={id}>
-							<Link to={url}>{text}</Link>
+							<Link to={url} onClick={closeSidebar}>
+								{text}
+							</Link>
 						</li>
 					))}
 					<li>
-						<Link to='/checkout'>checkout</Link>
+						<Link to='/checkout' onClick={closeSidebar}>
+							checkout
+						</Link>
 					</li>
 				</ul>
 				<CartButtons />
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -13,8 +13,8 @@ const products_reducer = (state, action) => {
 	if (action.type === SIDEBAR_OPEN) {
 		return { ...state, isSidebarOpen: true };
 	}
-  if (action.type === SIDEBAR_OPEN) {
-		return { ...state, isSidebarOpen: true };
+	if (action.type === SIDEBAR_CLOSE) {
+		return { ...state, isSidebarOpen: false };
 	}
 	throw new Error(`No Matching "${action.type}" - action type`);
 };
